deef612693d8f1f88a9e706f8b125c913df37370
Al-Amin <129585370+alaminbepari@users.noreply.github.com>
2023-12-13 18:02:11 +0600

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const object = {
+  Name :"Shuvo Niloy",
+  age :23,
+  email :"shuvoniloy7@gmail.com",
+  userData : {
+    logedin:true,
+    logedOut:false
+  }
+}
+
+// console.log(object.userData?.logedin);
+
+const arry = ["hello1","hello 1","Hello1"];
+const arry2 = ["hello2","hello 2","Hello2"];
+const arry22 = ["hello2","hello 2","Hello2"];
+
+const arry4 = [...arry, ...arry2, ...arry22]
+console.log( arry4);
+hello
